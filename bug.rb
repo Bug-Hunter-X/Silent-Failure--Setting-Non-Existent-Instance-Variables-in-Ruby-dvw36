cbe6e81fff60d2661fd2f780cb5c38d3aa89e999
BugHunter-X <askzenai@gmail.com>
2024-12-13 17:36:06 +0100

    Add bug.rb file

---
+++ bug.rb
@@ -0,0 +1,21 @@
+```ruby
+class MyClass
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+end
+
+my_object = MyClass.new(10)
+puts my_object.value #=> 10
+
+my_object.instance_variable_set(:@value, 20)
+puts my_object.value #=> 20 
+
+# This will NOT throw an error
+my_object.instance_variable_set(:@nonexistent_variable, 30)
+puts my_object.value #=> 20
+```
